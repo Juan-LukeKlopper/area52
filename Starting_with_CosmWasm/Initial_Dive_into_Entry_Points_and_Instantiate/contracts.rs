99740ae70b4d70de416433a95236d509c401cba4
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-06-26 22:26:00 +0200

    feat: Add response to instantiate fn

--- Starting_with_CosmWasm/Initial_Dive_into_Entry_Points_and_Instantiate/contracts.rs
+++ Starting_with_CosmWasm/Initial_Dive_into_Entry_Points_and_Instantiate/contracts.rs
@@ -1,4 +1,8 @@
 use cosmwasm_std::entry_point;
+use cosmwasm_std::{Response};
+use crate::error::ContractError;
 
 #[entry_point]
-pub fn instantiate() {}
+pub fn instantiate() -> Result<Response, ContractError> {
+        Ok(Response::default())
+}
