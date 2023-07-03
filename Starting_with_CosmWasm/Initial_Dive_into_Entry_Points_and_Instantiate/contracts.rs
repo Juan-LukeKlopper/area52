a2c3543d9313904903f97079f2eca82b3c290e28
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-03 20:28:44 +0200

    feat: Add custom response to contract.rs

--- Starting_with_CosmWasm/Initial_Dive_into_Entry_Points_and_Instantiate/contracts.rs
+++ Starting_with_CosmWasm/Initial_Dive_into_Entry_Points_and_Instantiate/contracts.rs
@@ -5,7 +5,6 @@ use crate::error::ContractError;
 use crate::msg::{InstantiateMsg};
 use crate::state::{config, State};
 
-#[entry_point]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
@@ -13,10 +12,14 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     let state = State {
-        owner: info.sender,
+        owner: info.sender.clone(),
+        planet_name: msg.planet_name,
+        planet_sapients: msg.planet_sapients,
+        minimum_sapience: msg.minimum_sapience,
     };
-
     config(deps.storage).save(&state)?;
-
-    Ok(Response::default())
+    
+    Ok(Response::new()
+        .add_attribute("owner", state.owner)
+        .add_attribute("minimum_sapience", state.minimum_sapience.as_str()))
 }
