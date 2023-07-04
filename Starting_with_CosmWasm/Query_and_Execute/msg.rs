bf627ccf22c9464ffe781fa696bb44900cd6c2c8
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-04 14:52:18 +0200

    feat: Add execute enum to msg.rs

--- Starting_with_CosmWasm/Query_and_Execute/msg.rs
+++ Starting_with_CosmWasm/Query_and_Execute/msg.rs
@@ -2,6 +2,13 @@ use serde::{Deserialize, Serialize};
 use universe::species::{SapienceScale, Sapient};
 
 #[derive(Serialize, Deserialize)]
+pub enum ExecuteMsg {
+    SetPlanetName { to: String },
+    SetSapientNames { to: Vec<Sapient> },
+    SetMinimumSapience { to: SapienceScale },
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct InstantiateMsg {
     pub planet_name: String,
     pub planet_sapients: Vec<Sapient>,
