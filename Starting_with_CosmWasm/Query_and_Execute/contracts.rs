d852bfbf3b4bbd7f3f8b66edc972ffe81195a66b
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-04 14:38:05 +0200

    chore: Move project to new folder for second section of tut

---
+++ Starting_with_CosmWasm/Query_and_Execute/contracts.rs
@@ -0,0 +1,25 @@
+use cosmwasm_std::{
+    entry_point, DepsMut, Env, MessageInfo, Response
+};
+use crate::error::ContractError;
+use crate::msg::{InstantiateMsg, ExecuteMsg};
+use crate::state::{config, State};
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    let state = State {
+        owner: info.sender.clone(),
+        planet_name: msg.planet_name,
+        planet_sapients: msg.planet_sapients,
+        minimum_sapience: msg.minimum_sapience,
+    };
+    config(deps.storage).save(&state)?;
+    Ok(Response::new()
+        .add_attribute("owner", state.owner)
+        .add_attribute("minimum_sapience", state.minimum_sapience.as_str()))
+}
