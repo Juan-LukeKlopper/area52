b18ccf649905f4525f7eca4df6a9e7d260ad2b33
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-04 14:38:59 +0200

    feat: Add execute entry_point

--- Starting_with_CosmWasm/Query_and_Execute/contracts.rs
+++ Starting_with_CosmWasm/Query_and_Execute/contracts.rs
@@ -6,6 +6,15 @@ use crate::msg::{InstantiateMsg, ExecuteMsg};
 use crate::state::{config, State};
 
 #[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+}
+
+#[entry_point]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
