9801adc6d9cd02e3151615e7cdf0b7059b729ff7
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-05 15:38:06 +0200

    feat: Add result to imbile_potion fn

--- Starting_with_CosmWasm/Messages_between_contracts/execute_fns/imbibe_potion.rs
+++ Starting_with_CosmWasm/Messages_between_contracts/execute_fns/imbibe_potion.rs
@@ -9,5 +9,12 @@ pub fn imbibe_potion(
     deps: DepsMut,
     info: MessageInfo,
 ) -> Result<Response, ContractError> {
-    // imbibe_potion code needs to added here
+    let mut state = config(deps.storage).load()?;
+    let swigs = state.swigs;
+
+    if swigs == 0 {
+        return Err(ContractError::OutOfSwigs {});
+    }
+    state.swigs = swigs - 1;
+    config(deps.storage).save(&state)?; 
 }
