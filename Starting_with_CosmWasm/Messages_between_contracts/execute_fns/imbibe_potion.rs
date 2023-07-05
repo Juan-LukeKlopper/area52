49e805e63ef7ff653e88a5dc15663191e8ca523a
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-05 15:52:15 +0200

    feat: Add Imbiber and save it to state

--- Starting_with_CosmWasm/Messages_between_contracts/execute_fns/imbibe_potion.rs
+++ Starting_with_CosmWasm/Messages_between_contracts/execute_fns/imbibe_potion.rs
@@ -16,5 +16,18 @@ pub fn imbibe_potion(
         return Err(ContractError::OutOfSwigs {});
     }
     state.swigs = swigs - 1;
-    config(deps.storage).save(&state)?; 
+    config(deps.storage).save(&state)?;
+
+    let cyborg_dna = b"123ABC".to_vec();
+
+    let cyborg = Imbiber {
+        address: info.sender.clone(),
+        species: species.clone(),
+        name: name.clone(),
+        cyborg_dna: cyborg_dna,
+    };
+    
+    let key = info.sender.to_string();
+
+    imbiber(deps.storage).save(key.as_bytes(), &cyborg)?;
 }
